9dd408509b3ffcb63b1eea7ef594641e92ea8cff
onwuegbucb <onwuegbucb@coventry.ac.uk>
2020-11-18 11:33:07 +0100

    Add project files.

---
+++ SignUp3/SignUp3/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SignUp3
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void LoginBtn_Clicked(object sender, EventArgs e) //Click button event//
+        {
+            if (string.IsNullOrEmpty(EntUserName.Text) || string.IsNullOrEmpty(EntPass.Text))
+            {
+                await DisplayAlert("Message", "Empty fields", "Ok");
+            }
+            else
+            {
+                await DisplayAlert("Message", $"Registered {EntUserName.Text}", "Ok");
+                EntUserName.Text = string.Empty;
+                EntPass.Text = string.Empty;
+            }
+        }
+
+        private async void SignUp_Clicked(object sender, EventArgs e)  //Sign Up click button event//
+        {
+            if (string.IsNullOrEmpty(NewEmail.Text) || string.IsNullOrEmpty(NewUser.Text) || string.IsNullOrEmpty(NewPass.Text) || string.IsNullOrEmpty(ConfirmPass.Text))
+            {
+                await DisplayAlert("Message", "Empty fields", "Ok");
+            }
+            else
+            {
+                if (ConfirmPass.Text != NewPass.Text)
+                {
+                    await DisplayAlert("Message", "Password does not match", "Ok");
+                }
+                else
+                {
+                    await DisplayAlert("Welcome", $"Registered {EntUserName.Text}", "Ok");
+                    NewPass.Text = string.Empty;
+                    NewEmail.Text = string.Empty;
+                    NewUser.Text = string.Empty;
+                    ConfirmPass.Text = string.Empty;
+                }
+            }
+        }
+    }
+}
